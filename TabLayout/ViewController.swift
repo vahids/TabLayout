b4f698fb05f3379820a4649dc918332cad97329c
Vahid Sayad <vahidgiga@gmail.com>
2018-03-16 17:56:41 +0330

    Initial Commit

---
+++ TabLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabLayout
+//
+//  Created by Vahid Sayad on 25/12/1396 .
+//  Copyright © 1396 Vahid Sayad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
